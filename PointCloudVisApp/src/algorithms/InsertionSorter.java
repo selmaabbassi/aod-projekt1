c485245ea490625824afa44ebe8ba795b2403328
Selma Abbassi <selma.abbassi@lm.se>
2023-12-04 12:41:07 +0100

    Implemented InsertionSorter

---
+++ PointCloudVisApp/src/algorithms/InsertionSorter.java
@@ -0,0 +1,37 @@
+package algorithms;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Insertion sorting algorithm
+ * @param <T> Generic type that extends @{@link Comparable}
+ * @author Selma Abbassi
+ */
+public class InsertionSorter<T extends Comparable<? super T>> implements Sorter<T> {
+
+        /**
+         * Sorts a @{@link List} using @{@link Comparable#compareTo(Object)} in lower to higher order
+         * @param list of generic types that extend @{@link Comparable}
+         */
+        @Override
+        public void sort(List<T> list) {
+                int high = list.size();
+                int low = 0;
+
+                for (int i = low + 1; i < high; i++) {
+                        for (int k = i; k >= low + 1; k--) {
+                                T current = list.get(k);
+                                T previous = list.get(k - 1);
+                                if(current.compareTo(previous) < 0) {
+                                        Collections.swap(list, list.indexOf(current), list.indexOf(previous));
+                                }
+                        }
+                }
+        }
+
+        @Override
+        public String toString() {
+                return getClass().toString();
+        }
+}
---
+++ PointCloudVisApp/test/algorithms/InsertionSorterTest.java
@@ -0,0 +1,22 @@
+package algorithms;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InsertionSorterTest {
+
+        @BeforeEach
+        void setUp() {
+        }
+
+        @AfterEach
+        void tearDown() {
+        }
+
+        @Test
+        void name() {
+        }
+}
